4855b1a990f7446c7e5a159075685516f0a60a42
Mihail <sidorov.mihail.maykop@mail.ru>
2020-02-10 16:05:04 +0300

    Удалил лишние файлы и скрипты.

--- index.php
+++ index.php
@@ -7,9 +7,7 @@
     
     <title>Где купить Karmy</title>   
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- <link rel="stylesheet" type="text/css" href="https://yastatic.net/bootstrap/3.3.4/css/bootstrap.min.css"> -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css"> 
-    <!-- <link rel="stylesheet" type="text/css" href="node_modules/motion-ui/dist/motion-ui.min.css"> -->
     <link rel="stylesheet" type="text/css" href="css/style.css">  
     <link rel="stylesheet" type="text/css" href="css/new-style.css">
 
@@ -18,8 +16,6 @@
   <body ng-controller="myCtrl">
 
     <div class="header">
-      <!-- <h1>Karmy YmapsAPI Test</h1> -->
-
       <a href="http://karmypet.ru" class="logo"><img src="img/logo.png" alt=""></a>
 
       <ul class="menu">
@@ -108,58 +104,6 @@
       </div>
     </div>
 
-
-    <div class="wrapper map-sidebar-wrapper">     
-      <div class="map-btn"><span class="btn-title">Показать список магазинов</span></div>
-      <!-- Here goes Map container -->
-      <div id="map" class="map-container">
-      </div>
-      
-      <!-- Here goes Sidebar container -->
-      <div class="sidebar">
-        <div class="sidebar-block cities-block">        
-          <h2 class="sidebar-title"><span>Ваш город:</span><span class="sidebar-title-city">{{city.Name}}</span>
-            <div class="sidebar-close"></div>
-          </h2>         
-        </div> 
-
-        <div class="sidebar-block shops-block">        
-          <ul ng-repeat="cityShop in cityShops" ng-class="{'active':$first}" city-id="{{$index}}">
-            <li>{{cityShop.name}}</li>
-            <li>{{cityShop.street_type}} {{cityShop.street}}, {{cityShop.house}}</li>
-            <li>{{cityShop.hours}}</li>
-            <li>{{cityShop.phone}}</li>
-            <li><a href="{{cityShop.site}}" target="_blank">{{cityShop.site}}</a></li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="choose-city-wrapper" tabindex="0">
-        <div class="preloader"></div>
-        <div class="auto-city">
-          <h2>Ваш город - {{autoCity.Name}}?</h2>
-          <div class="btns">
-            <button class="other-city" ng-click="showCityPopup('enter-city')">Выбрать другой</button>
-            <button class="this-city" ng-click="confirmAutoCity()">Да, это мой город</button>
-          </div>
-        </div>
-        <div class="enter-city">
-          <div class="back-btn"></div>
-          <h2>Начните вводить название города</h2>
-          <div class="enter-city-container">
-            <div class="enter-city-container-controls">
-              <input type="text" name="search-city-name" ng-model="searchCityName" ng-change="searchCityByName()" value="">
-              <button ng-click="confirmSearchCity()">Да, это мой город</button>
-              <div class="enter-city-container-controls-search-cities">
-                <div class="search-city" ng-repeat="searchCity in searchCities">{{searchCity.Name}}</div>
-              </div>
-              <div class="enter-city-container-controls-error">{{searchError}}</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
     <!-- Modal -->
     <div class="modal fade" id="get-call-modal" tabindex="-1" role="dialog" aria-hidden="true">
       <div class="get-call-modal-close"></div>
@@ -206,7 +150,6 @@
     <script src="js/mapSidebarCtrl.js" type="text/javascript" charset="utf-8"></script>
     <script src="js/sidebar.js"></script>
     <script src="js/mapCtrl.js" type="text/javascript" charset="utf-8"></script>
-    <!-- <script src="node_modules/motion-ui/dist/motion-ui.min.js"></script> -->
 
     <script src="js/menu.js"></script>
     <script src="js/mobile-menu.js"></script>
@@ -225,12 +168,7 @@
       gtag('config', 'UA-140125983-2');
       
       setTimeout(function(){
-          
-          //console.log($('form'));
-          
           $('#get-call-modal form').append('<input type="hidden" name="ga_cid-ga_cid-!" value='+ga.getAll()[0].get('clientId')+'>');
-          
-          //console.log(ga.getAll()[0].get('clientId'));
       }, 3000)
     </script>
     

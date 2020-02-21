2c88ab8f203b1ac3f5807fe2c57d936485c24388
Mihail <sidorov.mihail.maykop@mail.ru>
2020-02-21 17:53:58 +0300

    Добавил mutationobserver к блоку modal-window.

--- js/modal-window.js
+++ js/modal-window.js
@@ -1,3 +1,10 @@
+$('.modal-window').each(function(index, element){
+    var observer = new MutationObserver(function(res){
+        console.log($(res[0].target).attr('class'));
+    });
+    observer.observe(element, {attributeFilter: ['class']});
+});
+
 $('.modal-window__wrapper').click(function(e){
     if (e.target === this) {
         $(this).parent().removeClass('modal-window_show');

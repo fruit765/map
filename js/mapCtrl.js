08e25fe8688ac0a2dc519cd9a1b56af9dfdb147f
Mihail <sidorov.mihail.maykop@mail.ru>
2020-07-31 09:44:31 +0300

    Добавил новые точки.

--- js/mapCtrl.js
+++ js/mapCtrl.js
@@ -345,7 +345,7 @@ myApp.controller('myCtrl', function($scope, $http) {
         .then(function(response) {
             $scope.cities = response.data["cities"];
             /* Preload Shop Points */
-            $scope.city = $scope.cities[198];
+            $scope.city = $scope.cities[199];
         
             /* Run AJAX POST Shop Points request */
             $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
@@ -397,7 +397,7 @@ myApp.controller('myCtrl', function($scope, $http) {
                             $scope.showCityPopup('auto-city');
                         },
                         function(){
-                            $scope.autoCity = $scope.cities[198];
+                            $scope.autoCity = $scope.cities[199];
                             $scope.$apply();
                             $scope.showCityPopup('auto-city');
                         }
--- js/mapSidebarCtrl.js
+++ js/mapSidebarCtrl.js
@@ -25,7 +25,7 @@ function getCityObject(city, cities){
     });
 
     if (!cityResult) {
-        cityResult = cities[198];
+        cityResult = cities[199];
     }
 
     return cityResult;

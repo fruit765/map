a16dc1e9806e2717aae63378a3a27b03bd18378f
/usr/bin/vim <support@beget.ru>
2021-04-22 09:56:01 +0300

    Удалил счетчик Яндекс метрики.

--- index.php
+++ index.php
@@ -248,22 +248,5 @@
           $('#get-call-modal form').append('<input type="hidden" name="ga_cid-ga_cid-!" value='+ga.getAll()[0].get('clientId')+'>');
       }, 3000)
     </script>
-    
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
-      (function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
-      m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-      ym(71099074, "init", {
-            trustedDomains:["karmypet.ru"],
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-      });
-    </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/71099074" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-    <!-- /Yandex.Metrika counter -->
   </body>
 </html>
